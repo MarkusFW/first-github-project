bf64b14cba6b37ff9263c6800692240d7b57d5cc
MarkusFW <markusfw02@gmail.com>
2022-12-05 22:46:31 +0100

    test

---
+++ JS Final Project/movies.js
@@ -0,0 +1,4 @@
+// First Goal is : When search fast, show the first 6 movie titles and posters with fast.
+// Second goal is to: add  a skeleton loading stage
+
+// https://www.omdbapi.com/?apikey=2817c82&s=fast
